9e50bef4c526862e9f6432ba2ed0a667797db848
pradier.j <pradier.j@fr.ibm.com>
2019-07-15 11:25:03 +0200

    Adding sample addConsent call and reviewing the UI

--- app.js
+++ app.js
@@ -33,5 +33,5 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
 })
 app.listen(port, () => {
-    console.log('Server stated on port ' + port);
+    console.log('Server started on port ' + port);
 });
